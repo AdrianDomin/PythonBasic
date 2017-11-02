c13495172ce6f64cd474a7331d0188065179de10
AdrianDomin <adriandominn@gmail.com>
2017-11-02 12:23:21 +0100

    zmiany w .py

--- SzanseWlotto.py
+++ SzanseWlotto.py
@@ -2,4 +2,13 @@ import random
 zakres= range(1,49)
 LosoweLiczby = tuple(random.sample(zakres,6))
 
-
+WybraneLiczby = [0,0,0,0,0,0]
+try:
+    for i in range (6):
+        WybraneLiczby[i]= int(input("podaj {} liczbę: ".format(i+1)))
+        if WybraneLiczby[i] <1 or WybraneLiczby[i]>=49:
+            print ("liczba nie zawiera sie w zakresie 1-49!")
+            break
+    print ("Wybrane liczby to : {}".format(WybraneLiczby))
+except ValueError:
+    print ("podawaj liczby z zakresu 1-49!")
